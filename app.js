64a1ce904bfaa2b2aabdc0320baae1111e79aa50
Sridhar <sridharmoturu@gmail.com>
2024-01-09 12:15:37 +0530

    some changes

--- app.js
+++ app.js
@@ -5,36 +5,26 @@ dotenv.config()
 import 'express-async-errors'
 import cors from 'cors'
 
-import Routes from './Routes/books.js'
-
 const app = express()
-app.use( express.json() );
-// this will allow every request to come in and every method
-app.use(cors())
-/* we can restrict requests and methods which are came to the server
-    origin : give access to only specified origins
-    methods: requested url can perform specified functions only
-    headers : I don't about this
-*/
-// app.use( cors( 
-//     {
-//         origin: '',
-//         methods: [ GET, POST, UPDATE, DELETE ],
-//         allowedHeaders: ['Content-Type']
-//     }
-//     ))
+// Imports from another files
+import Routes from './Routes/books.js'
 
 app.get('/', (req, res) => {
     res.send('<h1>Welcome to BookStore</h1>')
 })
-    /* Middleware */
-app.use( '/books', Routes)
+
+/* Middlewares */
+app.use( express.json() );
+// this will allow every request to come in and every method
+app.use(cors())
+app.use('/api/books', Routes)
 
 const PORT = process.env.PORT || 5000;
 
 const start = async()=> {
  try {
     await mongoose.connect(process.env.MONGO_URL)
+    console.log('Connected to DataBase')
     app.listen(PORT, ()=> console.log(`Server is started on Port ${PORT}`))
  } catch (error) {
     console.log(error.message)
